66de6381fca169b382764fa532e2e1ba8a95f067
Данила Печенин <dpechenin@icloud.com>
2024-06-09 16:08:15 +0300

    Initial Commit

---
+++ traversing_bin_tree/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  traversing_bin_tree
+//
+//  Created by Данила Печенин on 09.06.2024.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
